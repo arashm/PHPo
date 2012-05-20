2a61e2033eb00a7581c8cbc7a477187bc010e68e
Arash Mousavi <mousavi.arash@gmail.com>
2012-05-20 22:33:35 +0430

    Add support for getStatementsById()

--- test.php
+++ test.php
@@ -3,14 +3,16 @@ require_once 'PHPo/PHPo.php';
 
 $po = new PHPo('test.po');
 
-//print_r($po->getHeader());
+// print_r($po->getHeader());
 
 // print_r($po->getStatements());
 
-//print_r($po->getStatements("fuzzy"));
+// print_r($po->getStatements("fuzzy"));
 
 // print_r($po->getTranslationPercentage());
 
-echo ($po->getStatementsByCount("untranslated"));
+// echo ($po->getStatementsByCount("untranslated"));
+
+print_r($po->getStatementsById(2));
 
 // echo $po;
